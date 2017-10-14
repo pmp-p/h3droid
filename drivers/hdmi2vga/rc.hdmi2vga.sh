cf8e2766c3b9a79cc4c3c9a5baa5c35d278a7edb
pmp-p <pmp-p@users.noreply.github.com>
2017-10-14 20:11:21 +0200

    automatic delay

--- drivers/hdmi2vga/rc.hdmi2vga.sh
+++ drivers/hdmi2vga/rc.hdmi2vga.sh
@@ -1,8 +1,32 @@
 #!/system/bin/sh
+
+dumpsys input_method | grep  "mSystemReady="
+
+while true
+do
+    if dumpsys power 2>/dev/null|grep -q "mSystemReady=true"
+    then
+        break
+    else
+        echo "Waiting user interface ..."
+        date
+        sleep 2
+    fi
+done
+
+sleep 5
+
 if dmesg |grep 'ddc read timeout'|wc -l|grep -q 128
 then
-    input keyevent KEYCODE_POWER
-    sleep 1
-    input keyevent KEYCODE_POWER
+    echo "    DDC error detected"
+    if dumpsys power|grep -q "mScreenOn=true"
+    then
+        echo "   1/2 - sending screen to sleep"
+        input keyevent --shortpress KEYCODE_POWER
+        sleep 0.8
+    fi
+    echo "    2/2 - waking up screen"
+    input keyevent --shortpress KEYCODE_POWER
 fi
 
+#   adb shell input keyevent 82 # Unlock screen
