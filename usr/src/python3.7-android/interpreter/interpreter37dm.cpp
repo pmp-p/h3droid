d031f366b5e90ceb78bc96c113ce2e8d87a148ad
pmp-p <pmp-p@users.noreply.github.com>
2017-12-01 11:52:38 +0100

    minimal repl, old fashion
     py_main has problem with wcslen() on api-19

---
+++ usr/src/python3.7-android/interpreter/interpreter37dm.cpp
@@ -0,0 +1,17 @@
+#define INTERPRETER 1
+#define REPL 1
+
+static char *root_folder = "/data/data/u.r";
+
+#define PY_PATH "/data/data/u.r/usr/lib/python3.7"
+#define PYTHONPATH "/data/data/u.r/usr/lib/python3.7/lib-dynload:/data/data/u.r/usr/lib/python3.7:/data/data/u.r/usr/lib/python3"
+#define LIB_PYTHON "libpython3.7dm.so"
+#define PY_LIB "/data/data/u.r/usr/lib/python3.7"
+#define PY_LIBS "/data/data/u.r/usr/lib/python3.7:/data/data/u.r/usr/lib/python3"
+
+#define PYVER 37
+
+#define PY_LOG "Python3.7dm"
+
+#include "cpython3xdm.cpp"
+
